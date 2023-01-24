bfda6fbdf7d88c51e5ff24d01bf3f59bc78bebd1
unknown <uzbking0626@gmail.com>
2023-01-24 15:02:32 +0300

    Calculator

---
+++ 32.js
@@ -0,0 +1,8 @@
+function count() {
+            var num1 = document.getElementById("num1").value;
+            var num2 = document.getElementById("num2").value;
+            var mod = document.getElementById("mod").value;
+            document.getElementById('result').value = eval(num1 + mod + num2);
+          }
+
+
